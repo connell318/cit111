064bc68aed22ad14678597a9743f127ff96575c7
connell318 <43351633+connell318@users.noreply.github.com>
2018-11-05 13:46:29 -0500

    Add files via upload

---
+++ Donut.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproj;
+
+/**
+ *"blueprint class for
+ * modeling a real life
+ * donut in Java
+ * @author ryan.connell
+ */
+public class Donut {
+    //  member variables
+    // that all Donut objects have
+String Taste;
+String topping;
+String color;  
+String smell;
+String name;
+
+}//close calss
---
+++ KennywoodRideTracker.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ls3_classStructure.newpackage;
+
+/**
+ *
+ * @author ryan.connell
+ */
+public class KennywoodRideTracker {
+    public static void main(String[] args){
+    }//close main ()
+     public static void rideBlackWidow(int riders, int avgHeight){
+         if(avgHeight >= MIN_HEIGHT_THRILL_RIDES){
+             
+             System.out.println("Riding Black Widow...");
+             System.out.println("I HAAAATE SPIDERS!");
+             totalRiders = totalRiders + riders;
+             Random rand = new Random();
+             int numSickRiders = rand.nextInt(riders);
+             totalSick
+         }
+     }//close BlackWidow
+     public static void rideMerryGoRound(int riders, int avgHeight){
+         
+     }//close MerryGoRound
+    public static void printRiderStats(){
+        
+    }//close RiderStats
+    
+    
+}//close class
